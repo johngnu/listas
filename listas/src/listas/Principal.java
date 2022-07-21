5a4bbd5daf2bd192c9e63aa9294fb5be9d24daa7
John Castillo <john.gnu@gmail.com>
2022-07-21 01:05:14 -0400

    init

---
+++ listas/src/listas/Principal.java
@@ -0,0 +1,13 @@
+package listas;
+
+/**
+ * INF - 131
+ * @author helen carolina castillo mamani
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
